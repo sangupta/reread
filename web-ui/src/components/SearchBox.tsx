b76acd01f340280a4282efbc105f7df72c8fd98a
Sandeep Gupta <sandy.pec@gmail.com>
2021-05-11 20:02:28 -0700

    fix search box width in medium width screens

--- web-ui/src/components/SearchBox.tsx
+++ web-ui/src/components/SearchBox.tsx
@@ -36,7 +36,7 @@ class SearchBox extends React.Component<SearchBoxProps, SearchBoxState> {
     render() {
         const { text } = this.state;
 
-        return <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" onSubmit={this.runSearch}>
+        return <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3 col-md-2" onSubmit={this.runSearch}>
             <input type="search" className="form-control form-control-dark" placeholder="Search..." aria-label="Search" value={text} onChange={this.setText} />
         </form>
     }
